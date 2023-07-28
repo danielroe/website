f21b6165cb20240567c566d8746f5bc363168b5d
bryantgillespie <hey@bryantgillespie.com>
2023-07-28 13:27:58 -0400

    code snippet vue comp

---
+++ components/Comp/CodeSnippet.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { CompProps } from './types';
+
+const { $directus, $readItem } = useNuxtApp();
+
+const props = defineProps<CompProps>();
+
+const { data: comp } = useAsyncData(props.uuid, () =>
+	$directus.request(
+		$readItem('comp_code_snippets', props.uuid, {
+			fields: ['items'],
+		})
+	)
+);
+</script>
+
+<template>
+	<div class="code-snippets"></div>
+</template>
